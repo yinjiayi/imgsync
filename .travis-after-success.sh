83378a5684185b9f79779bfc838e1472ef979add
YIN JIA YI <yinjiayi@live.cn>
2020-10-17 21:57:56 +0800

    Update .travis-after-success.sh

--- .travis-after-success.sh
+++ .travis-after-success.sh
@@ -7,5 +7,5 @@ cd ${GCR_REPO}
 if [ -n "$(git status --porcelain)" ]; then
   git add .
   git commit -m "Travis CI Auto Update(`date +'%Y-%m-%d %H:%M:%S'`)"
-  git push https://mritd:${GITHUB_TOKEN}@github.com/mritd/gcr.git
+  git push https://yinjiayi:${GITHUB_TOKEN}@github.com/yinjiayi/gcr.git
 fi
